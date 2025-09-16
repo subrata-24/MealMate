24e120345e5c8b7574b25989114dec3311d4dd19
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-17 04:14:18 +0600

    feat: Make connection with MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server is running at http://localhost:${PORT}`);
 });
