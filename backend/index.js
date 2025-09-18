372467d627c5c760a532341eb54d1dad0fdcd795
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-18 19:31:35 +0600

    chore(app): configure middleware for CORS, JSON parsing, and cookies
    - Enable CORS for frontend at http://localhost:5173 with credentials support
    - Add express.json() to parse incoming JSON request bodies
    - Add cookie-parser to handle JWT cookies in requests
    - Mount auth routes under /api/auth

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 dotenv.config();
+
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    Credential: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectDB();
