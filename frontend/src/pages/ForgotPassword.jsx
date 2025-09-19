dc3e2489d6121c6b14ba0944eef1ec0e73e66d81
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-19 23:40:34 +0600

    feat(auth): add step 1 of forget password flow with email input and Send OTP button

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
   const [step, setStep] = useState(1);
+  const [email, setEmail] = useState(" ");
+  const navigate = useNavigate();
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-[#fff9f6] p-4">
       <div className="w-full max-w-md p-8 rounded-lg shadow-2xl">
@@ -10,11 +13,35 @@ const ForgotPassword = () => {
           <IoMdArrowRoundBack
             size={25}
             className="text-[#ff4d2d] font-bold cursor-pointer"
+            onClick={() => navigate("/signin")}
           />
           <h1 className="text-2xl font-bold text-[#ff4d2d] text-center">
             Forgot Password
           </h1>
         </div>
+
+        {step === 1 && (
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full rounded-lg px-4 py-3 border border-gray-200 bg-white text-gray-800 placeholder-gray-500 shadow-sm focus:border-orange-500 focus:ring-2 focus:ring-orange-300 transition-all"
+            />
+            <div className="flex justify-center mt-4">
+              <button className="bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600 text-white font-semibold py-3 px-39 rounded-lg shadow-lg hover:shadow-xl transform hover:scale-[1.03] transition-all duration-300 cursor-pointer">
+                Send OTP
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
