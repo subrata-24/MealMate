321cf8f1520d9246d06a9eee0dcae1eda0b115f8
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-22 01:24:45 +0600

    fix: fix the missing  extension(.js) when they are imported

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 
 // “Who is currently logged in, and what is their info?”
 // Provide a secure way to fetch the currently logged-in user’s information using the JWT, without requiring the user to log in again.The user may refresh the page, open the app later, or return after some time.The frontend no longer has the user info in memory — it only has the JWT cookie.After login, or on page refresh, the frontend can call this endpoint to get the user’s details(like name, email, role) based on the JWT stored in cookies.
