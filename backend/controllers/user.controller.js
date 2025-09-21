b6cb9c11b00466c1673f70a228a82390df48fb59
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-22 00:06:13 +0600

    feat(user): add getCurrentUser endpoint
    - Fetch current authenticated user using JWT
    - Validate userId from request
    - Return user info if exists, handle errors otherwise
    - Exclude sensitive fields (e.g., password) for security

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model";
+
+// “Who is currently logged in, and what is their info?”
+// Provide a secure way to fetch the currently logged-in user’s information using the JWT, without requiring the user to log in again.The user may refresh the page, open the app later, or return after some time.The frontend no longer has the user info in memory — it only has the JWT cookie.After login, or on page refresh, the frontend can call this endpoint to get the user’s details(like name, email, role) based on the JWT stored in cookies.
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(400).json({ message: "userId is not found" });
+    }
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return res.status(400).json({ message: "user not found" });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: `get current user error ${error}` });
+  }
+};
