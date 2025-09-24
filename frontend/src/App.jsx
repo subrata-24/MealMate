8d21dc1b1623fcb1b1150fedd89ed120a02943eb
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-25 01:29:58 +0600

    feat:Setup routing for home page and authentication redirects
    - Implemented conditional navigation based on user authentication state
    - Integrated `useGetCurrentUser` hook to fetch current user data
    - Redirect authenticated users away from auth pages
    - Redirect unauthenticated users to SignIn from Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import useGetCurrentUser from "./hooks/useGetCurrentUser";
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
 export const serverUrl = "http://localhost:8000";
 
 const App = () => {
   useGetCurrentUser();
+  const { userData } = useSelector((state) => state.user);
   return (
     <div>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/signup"
+          element={!userData ? <SignUp /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signin"
+          element={!userData ? <SignIn /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/forgot-password"
+          element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/"
+          element={userData ? <Home /> : <Navigate to={"/signin"} />}
+        />
       </Routes>
     </div>
   );
