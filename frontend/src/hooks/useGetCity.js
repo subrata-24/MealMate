8d467553179b93007f9898b0426210348cfde6c6
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-09-26 18:49:44 +0600

    feat(geolocation): add custom hook `useGetCity` to auto-detect and store user city
    * Created `useGetCity` hook that uses browser Geolocation API to get user coordinates
    * Calls Geoapify reverse geocoding API to convert coordinates to a city name
    * Dispatches `setCity` action to save the detected city in Redux store
    * Updates city automatically whenever `userData` changes
    * Enables dynamic location-based functionality across the app (e.g., search bar location)

---
+++ frontend/src/hooks/useGetCity.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setCity } from "../redux/userSlice";
+
+const useGetCity = () => {
+  const dispatch = useDispatch();
+  const apiKey = import.meta.env.VITE_GEOAPIKEY;
+  const { userData } = useSelector((state) => state.user);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      //   console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const result = await axios.get(
+        `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`
+      );
+
+      //   console.log(result);
+      //   const city = result.data.results[0].city;
+      //   console.log(city);
+      dispatch(setCity(result?.data?.results[0].city));
+    });
+  }, [userData]);
+};
+
+export default useGetCity;
