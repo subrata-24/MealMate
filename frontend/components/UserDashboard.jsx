be665e3c81439ca6c42df2a28daac6b9747801f1
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-10-02 01:45:45 +0600

    feat: add horizontal scrollable category carousel with arrow controls
    - Implemented left and right arrow buttons to scroll categories smoothly
    - Used useRef to reference the scrollable container
    - Styled buttons with absolute positioning, hover effects, and shadow
    - Enabled responsive horizontal scrolling for overflowing categories

--- frontend/components/UserDashboard.jsx
+++ frontend/components/UserDashboard.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "./Navbar";
 import { categories } from "../src/category";
 import CategoryCard from "./CategoryCard";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 const UserDashboard = () => {
+  const cateScrollRef = useRef();
+
+  const scrollHandler = (ref, direction) => {
+    if (ref.current) {
+      ref.current.scrollBy({
+        left: direction == "left" ? -200 : 200,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="w-screen min-h-screen flex flex-col gap-6 items-center bg-[#fff9f6] overflow-y-auto">
       {/* Navbar */}
@@ -16,12 +28,27 @@ const UserDashboard = () => {
         </h1>
 
         {/* Categories Grid */}
-        <div className="w-full">
-          <div className="w-full flex overflow-x-auto gap-4 pb-2">
+        <div className="w-full relative">
+          <button
+            className="absolute top-1/2 left-0 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+            onClick={() => scrollHandler(cateScrollRef, "left")}
+          >
+            <FaArrowAltCircleLeft />
+          </button>
+          <div
+            className="w-full flex overflow-x-auto gap-4 pb-2"
+            ref={cateScrollRef}
+          >
             {categories.map((cate, index) => (
               <CategoryCard data={cate} key={index} />
             ))}
           </div>
+          <button
+            className="absolute top-1/2 right-0 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 "
+            onClick={() => scrollHandler(cateScrollRef, "right")}
+          >
+            <FaArrowAltCircleRight />
+          </button>
         </div>
       </div>
     </div>
