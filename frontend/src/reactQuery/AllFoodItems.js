287ede7d6270b2c3e4b14d27afab8ba39217cd34
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-21 19:02:16 +0600

    feat(api): add React Query hook for fetching all food items
    - Create `allFoodItems` hook using `useQuery`
    - Fetch all food items from `/api/public/get-all-items`
    - Implement caching with 5min `staleTime` and 10min `cacheTime`
    - Disable refetch on window focus for better control

---
+++ frontend/src/reactQuery/AllFoodItems.js
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { serverUrl } from "../App";
+
+export const allFoodItems = () => {
+  return useQuery({
+    queryKey: ["allFoods"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${serverUrl}/api/public/get-all-items`);
+      return data.data;
+    },
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
+    refetchOnWindowFocus: false, // Don't refetch on tab switch
+  });
+};
