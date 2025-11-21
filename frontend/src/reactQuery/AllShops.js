89f2a8d7409954db1ff0ea5152c7b3db49e8cb32
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-21 19:08:23 +0600

    feat(api): add React Query hook to fetch all shops
    - Create `allShops` hook using `useQuery` for API call to fetch shops
    - Implement caching with 5min staleTime and 10min cacheTime
    - Disable refetch on window focus for controlled data fetching

---
+++ frontend/src/reactQuery/AllShops.js
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { serverUrl } from "../App";
+
+export const allShops = () => {
+  return useQuery({
+    queryKey: ["allShops"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${serverUrl}/api/public/get-all-shop`);
+      return data.data;
+    },
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10 * 60 * 1000,
+    refetchOnWindowFocus: false,
+  });
+};
