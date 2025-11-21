d9966d7a1d66c13385af25783e9554bc9d13ee1f
Subrata Biswas <subrata17@cse.pstu.ac.bd>
2025-11-21 18:32:45 +0600

    feat(api): add allShop controller to fetch all shops
    - Fetch all shops with Shop.find({})
    - Return 404 if no shops found
    - Return 200 with list of shops if successful
    - Handle errors with 500 status and error message

--- backend/controllers/public.controller.js
+++ backend/controllers/public.controller.js
@@ -1,13 +1,26 @@
 import Item from "../models/item.model";
+import Shop from "../models/shop.model";
 
 export const allFoods = async (req, res) => {
   try {
     const items = await Item.find({});
     if (!items) {
-      return res.status(400).json({ error: "Item is not found" });
+      return res.status(404).json({ error: "Item is not found" });
     }
     return res.status(200).json(items);
   } catch (error) {
-    return res.status(500).json(`Found error on fetching data ${error}`);
+    return res.status(500).json(`Found error on fetching items ${error}`);
+  }
+};
+
+export const allShop = async (req, res) => {
+  try {
+    const shops = await Shop.find({});
+    if (!shops) {
+      return res.status(404).json({ error: "Shop is not found" });
+    }
+    return res.status(200).json(shops);
+  } catch (error) {
+    return res.status(500).json(`Found error on fetching shop ${error}`);
   }
 };
